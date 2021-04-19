c1b7b9c6a88b919364fb462e1737bacd0f9f4b9f
lijeffrey02 <lijeffrey02@gmail.com>
2021-04-19 02:32:25 -0400

    Task 4

--- BSTree/BSTree.cpp
+++ BSTree/BSTree.cpp
@@ -15,3 +15,19 @@ std::string BSTree::get_debug_string(){
     std::to_string(root->getLeft()->getLeft()->getLeft()->getData());
 }
 
+void BSTree::setup(){
+    Node *n = new Node(10);
+    root = n;
+    n = new Node(20);
+    root->setRight(n);
+    Node *n2 = new Node(30);
+    n->setRight(n2);
+    n2 = new Node(25);
+    n->setLeft(n2);
+    n2 = new Node(5);
+    root->setLeft(n2);
+    n = new Node(3);
+    n2->setLeft(n);
+    n2 = new Node(7);
+    n->setLeft(n2);
+}
