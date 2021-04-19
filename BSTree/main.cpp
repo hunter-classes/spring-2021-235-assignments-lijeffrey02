a1307d149cca6e2fad35ba559dc315cfe84b46bf
lijeffrey02 <lijeffrey02@gmail.com>
2021-04-19 02:37:38 -0400

    Test task 2

--- BSTree/main.cpp
+++ BSTree/main.cpp
@@ -2,5 +2,13 @@
 #include "Node.h"
 
 int main(){
+    Node *n = new Node(20);
+    std::cout << n->getData() << "\n";
+    Node *n2 = new Node(30);
+    n->setLeft(n2);
+    n2 = new Node(40);
+    n->setRight(n2);
+    std::cout << n->getLeft()->getData() << "\n";
+    std::cout << n->getRight()->getData() << "\n";
     return 0;
 }
